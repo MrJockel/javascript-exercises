8af98d6a9982fbd0648727c30ffaeebbbd77da5a
MrJockel <Joachim.Woerz@outlook.com>
2022-04-05 15:03:50 +0400

    Update reverseString.js
    Did the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+
+console.log("Test");
+
+
+
+const reverseString = function(s) {
+
+    return s.split("").reverse().join("");
+
 
 };
+
+console.log(reverseString("Fick Dich!!!"));
 
 // Do not edit below this line
 module.exports = reverseString;
